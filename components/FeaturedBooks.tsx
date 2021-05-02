3f1fe1e599a73b9586ea7d70eecf4eaf06757b24
Dustin Lacewell <dlacewell@gmail.com>
2021-05-02 11:56:22 -0500

    Fix FeaturedBooks randomization

--- components/FeaturedBooks.tsx
+++ components/FeaturedBooks.tsx
@@ -2,39 +2,23 @@ import { BOOKS } from '../data/reading-lists/books'
 import { AUTHORS } from '../data/reading-lists/authors'
 import { auth } from '../utils/auth'
 import { Box, Image, VStack, HStack, Center, Heading, Container } from '@chakra-ui/react'
+import { FC, useEffect, useState } from 'react'
 
-const book_ids = Object.keys(BOOKS)
-const feats = (()=>{
-    const rand = () => {
-        return Math.floor(Math.random() * book_ids.length)
-    }
-    let rands = [rand(),rand(),rand()]
-    while(rands[0] == rands[1]){
-        rands[0] = rand()
-    }
-    while(rands[2] == rands[0] || rands[2] == rands[1]){
-        rands[2] = rand()
-    }
-    return [book_ids[rands[0]],book_ids[rands[1]],book_ids[rands[2]],]
-})()
 
-console.log(feats)
-for(let i=0; i<3; i++){
-  console.log(AUTHORS[BOOKS[feats[i]].author[0]])
+
+type BookProps = {
+    id: string;
+    book: any;
 }
 
-const Book = (n:number) => {
-    const feat = feats[n]
-    const book = BOOKS[feat]
-    const imgsrc = '/img/list-content/' + feat + '.jpg'
+const Book: FC<BookProps> = ({ id, book }) => {
+    const imgsrc = '/img/list-content/' + id + '.jpg'
     const booktitle = book.title
     const bookauthor = auth(AUTHORS[book.author[0]])
     try{
         return (
             <Box w="20vw">
                 <VStack>
-                    <p>{feats[n]}</p>
-                    <p>{imgsrc}</p>
                     <img style={{height: 300, width: 200}} src={imgsrc}/>
                     <Center align="center"><Heading size="md">{booktitle}</Heading></Center>
                     <Center align="center"><Heading size="xs">{bookauthor}</Heading></Center>
@@ -43,11 +27,27 @@ const Book = (n:number) => {
         )
     }
     catch(TypeError){
-        return <p>{feats[n]}</p>
+        return <p>{{ id, book}}</p>
     }
 }
 
 const FeaturedBooks = () => {
+    const book_ids = Object.keys(BOOKS)
+    book_ids.sort()
+
+    const pick = (idx?) => {
+        const index = idx || Math.floor(Math.random()*book_ids.length)
+        const id = book_ids.splice(index, 1)[0];
+        return {
+            id, book: BOOKS[id]
+        }
+    }
+
+    const [featured, setFeatured] = useState([]);
+    useEffect(() => {
+        setFeatured([pick(), pick(), pick()])
+    }, []) // randomize first time component is mounted
+
     return (
         <div style={{ paddingTop: "2vh", }}>
             <VStack spacing={5}>
@@ -55,9 +55,7 @@ const FeaturedBooks = () => {
                     <Heading size="xl">Some Books To Get You Started</Heading>
                 </Center>
                 <HStack spacing={5}>
-                    {Book(0)}
-                    {Book(1)}
-                    {Book(2)}
+                    {featured.map(({ id, book }) => <Book id={id} book={book} />)}
                 </HStack>
             </VStack>
         </div>
@@ -78,37 +76,37 @@ const FeaturedBooks = () => {
                     </Container>
                 </HStack>
             </VStack>
-        </div>        
+        </div>
     )*/
 }
 export default FeaturedBooks
 
-const FeatureList = [
-  {
-    title: BOOKS[feats[0]].title,
-    Jpg: `/img/list-content/${feats[0]}.jpg`,
-    description: (
-      <>
-        {auth(AUTHORS[BOOKS[feats[0]].author[0]])}
-      </>
-    ),
-  },
-  {
-    title: BOOKS[feats[1]].title,
-    Jpg: `/img/list-content/${feats[1]}.jpg`,
-    description: (
-      <>
-        {auth(AUTHORS[BOOKS[feats[1]].author[0]])}
-      </>
-    ),
-  },
-  {
-    title: BOOKS[feats[2]].title,
-    Jpg: `/img/list-content/${feats[2]}.jpg`,
-    description: (
-      <>
-        {auth(AUTHORS[BOOKS[feats[2]].author[0]])}
-      </>
-    ),
-  },
-];
+// const FeatureList = [
+//   {
+//     title: BOOKS[feats[0]].title,
+//     Jpg: `/img/list-content/${feats[0]}.jpg`,
+//     description: (
+//       <>
+//         {auth(AUTHORS[BOOKS[feats[0]].author[0]])}
+//       </>
+//     ),
+//   },
+//   {
+//     title: BOOKS[feats[1]].title,
+//     Jpg: `/img/list-content/${feats[1]}.jpg`,
+//     description: (
+//       <>
+//         {auth(AUTHORS[BOOKS[feats[1]].author[0]])}
+//       </>
+//     ),
+//   },
+//   {
+//     title: BOOKS[feats[2]].title,
+//     Jpg: `/img/list-content/${feats[2]}.jpg`,
+//     description: (
+//       <>
+//         {auth(AUTHORS[BOOKS[feats[2]].author[0]])}
+//       </>
+//     ),
+//   },
+// ];
